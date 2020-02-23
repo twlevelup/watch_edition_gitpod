42e501ad1377dc119f1f2f26ba7d2f05ad1e506a
Wayne Irwin <wayne.j.irwin@gmail.com>
2020-02-23 22:35:27 +1100

    Test using the framework from an node module

--- framework/PublicAudioHub.js
+++
@@ -1,7 +0,0 @@
-const AudioHub = require('./src/AudioHub');
-
-const AudioHubInstance = new AudioHub();
-
-module.exports = {
-  playSound: AudioHubInstance.playSound,
-}
--- framework/PublicNotificationHub.js
+++
@@ -1,6 +0,0 @@
-const NotificationHub = require('./src/NotificationHub');
-
-module.exports = {
-  show: NotificationHub.show,
-  hide: NotificationHub.hide,
-}
